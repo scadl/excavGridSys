488252acdab442c92d44177112ec8118c16d7ba9
Alexandr <scad.luncher@gmail.com>
2026-07-10 16:52:01 +0300

    Fully working grid system with toolbar

--- exgMain.py
+++ exgMain.py
@@ -23,11 +23,7 @@ class GridAnnotator:
         # Разрешаем менять размер окна
         self.master.resizable(True, True)
 
-        # Создаём второе окно — панель управления
-        self.control_window = tk.Toplevel(self.master)
-        self.control_window.title("Панель управления")
-        self.control_window.resizable(True, True)
-
+    
         # Оригинальное изображение
         self.image = None
         self.photo = None
@@ -38,49 +34,81 @@ class GridAnnotator:
         self.grid_top = None
         self.cell_width = None
         self.cell_height = None
+        
+        # timer refrecnce for detecting window resize
+        self.resize_job = None
 
-        self.letters = []
-        self.numbers = []
+        self.letters = ["A"]
+        self.numbers = ["1"]
         self.grid_data = {}
 
         self.click_stage = 0
-
+        
+        # Создаём панель управления
+        toolbar = ttk.Frame(self.master)        
+        ttk.Button(toolbar, text="Загрузить изображение", command=self.load_image).pack(side=tk.LEFT, padx=2)
+        ttk.Button(toolbar, text="Задать размер сетки", command=self.start_calibration).pack(side=tk.LEFT, padx=2)              
+        toolbar.pack(side=tk.TOP, fill=tk.X)
+              
         # Canvas для чертежа
         self.canvas = tk.Canvas(master, bg="gray")
         self.canvas.pack(fill=tk.BOTH, expand=True)
+        
+        # create image container
+        self.img_id = self.canvas.create_image(0, 0, anchor="nw", image=self.photo)
+        # drop container to lowest layer on canvas
+        self.canvas.tag_lower(self.img_id)
 
         # Обработчики
-        self.canvas.bind("<Button-1>", self.on_click)
         self.master.bind("<Configure>", self.on_resize)
-
-        # Кнопки — теперь в отдельном окне
-        ttk.Button(self.control_window, text="Загрузить изображение", command=self.load_image).pack(fill=tk.X)
-        ttk.Button(self.control_window, text="Начать калибровку сетки", command=self.start_calibration).pack(fill=tk.X)
-        ttk.Button(self.control_window, text="Сбросить сетку", command=self.reset_grid).pack(fill=tk.X)
-        ttk.Button(self.control_window, text="Сохранить JSON", command=self.save_json).pack(fill=tk.X)
+        
+        
+        self.create_handles()
 
     def create_handles(self):
+        
+        # REmove old handlers
+        self.canvas.delete("handle1")
+        self.canvas.delete("handle2")
+        
+        size = 50
+        
+        # Wait fow wind to draw. Required to get real size
+        self.master.update_idletasks()
+        
+        print("Canvas dimensions:", self.canvas.winfo_width(), self.canvas.winfo_height())
+        print("Window dimensions:", self.master.winfo_width(), self.master.winfo_height())
+        
         # Примерное стартовое положение (центр экрана)
-        cx = self.canvas.winfo_width() // 2
-        cy = self.canvas.winfo_height() // 2
+        if self.canvas.winfo_width() > size:
+            cx = self.canvas.winfo_width() // 2
+            cy = self.canvas.winfo_height() // 2
+        else:
+            cx = self.master.winfo_width() // 2
+            cy = self.master.winfo_height() // 2
 
         # Верхний левый угол
         self.handle1 = self.canvas.create_rectangle(
-            cx - 50, cy - 50,
-            cx - 50 + self.handle_size, cy - 50 + self.handle_size,
-            fill="yellow", outline="black", tags="handle"
+            cx - size, cy - size,
+            cx - size + self.handle_size, cy - size + self.handle_size,
+            fill="yellow", outline="black", tags="handle1"
         )
 
         # Нижний правый угол
         self.handle2 = self.canvas.create_rectangle(
-            cx + 50, cy + 50,
-            cx + 50 + self.handle_size, cy + 50 + self.handle_size,
-            fill="yellow", outline="black", tags="handle"
+            cx + size, cy + size,
+            cx + size + self.handle_size, cy + size + self.handle_size,
+            fill="yellow", outline="black", tags="handle2"
         )
         
-        self.canvas.tag_bind("handle", "<ButtonPress-1>", self.on_handle_press)
-        self.canvas.tag_bind("handle", "<B1-Motion>", self.on_handle_drag)
-        self.canvas.tag_bind("handle", "<ButtonRelease-1>", self.on_handle_release)
+        self.canvas.tag_bind("handle1", "<ButtonPress-1>", self.on_handle_press)
+        self.canvas.tag_bind("handle1", "<B1-Motion>", self.on_handle_drag)
+        self.canvas.tag_bind("handle1", "<ButtonRelease-1>", self.on_handle_release)
+        self.canvas.tag_bind("handle2", "<ButtonPress-1>", self.on_handle_press)
+        self.canvas.tag_bind("handle2", "<B1-Motion>", self.on_handle_drag)
+        self.canvas.tag_bind("handle2", "<ButtonRelease-1>", self.on_handle_release)
+        
+        self.update_grid_preview()
 
     def on_handle_press(self, event):
         # Определяем какой хендлер схвачен
@@ -89,6 +117,8 @@ class GridAnnotator:
             self.dragging_handle = item
 
     def on_handle_drag(self, event):
+        
+        # check if mouse dragging handel
         if not self.dragging_handle:
             return
 
@@ -122,6 +152,7 @@ class GridAnnotator:
 
         # Удаляем старую сетку
         self.canvas.delete("grid")
+        self.canvas.delete("letter")
 
         # Строим новую сетку
         for i, letter in enumerate(self.letters):
@@ -131,12 +162,27 @@ class GridAnnotator:
                 cx2 = cx1 + self.cell_w
                 cy2 = cy1 + self.cell_h
 
+                # allowed stipple: 12,25,50,75
                 self.canvas.create_rectangle(
                     cx1, cy1, cx2, cy2,
-                    outline="red", width=1, tags="grid"
+                    outline="red", width=1,
+                    fill="red", stipple="gray12",
+                    tags=("grid",f"{letter}{number}")
                 )
-
-
+                self.canvas.create_text(cx1 + 5, cy1 + 5, anchor="nw", 
+                                        text=f"{letter}{number}", fill="blue", 
+                                        tags=("grid",f"{letter}{number}"))
+                
+        self.rise_handeler_to_top()
+                
+        # Bind onMouseClick event (all mouse buttons)
+        self.canvas.tag_bind("grid", "<Button>", self.on_click)
+    
+    # Layer management, rise grid and handlers on top  
+    def rise_handeler_to_top(self):    
+        self.canvas.tag_raise("grid")
+        self.canvas.tag_raise("handle1")
+        self.canvas.tag_raise("handle2")
 
     # ------------------------------------------------------------
     # Загрузка изображения
@@ -152,7 +198,7 @@ class GridAnnotator:
     # ------------------------------------------------------------
     # Масштабирование изображения под окно
     # ------------------------------------------------------------
-    def update_scaled_image(self):
+    def update_scaled_image(self, fast=True):
         if self.image is None:
             return
 
@@ -166,28 +212,50 @@ class GridAnnotator:
         new_w = int(img_w * self.scale)
         new_h = int(img_h * self.scale)
 
-        resized = self.image.resize((new_w, new_h), Image.LANCZOS)
+        # apply resize to image itself, using one of the algos
+        if fast:
+            resized = self.image.resize((new_w, new_h), Image.NEAREST)
+        else:
+            resized = self.image.resize((new_w, new_h), Image.LANCZOS)
+        
+        # recrate image component in new size
         self.photo = ImageTk.PhotoImage(resized)
-
+        # insert component into form container (prepared during init)
+        self.canvas.itemconfig(self.img_id, image=self.photo)
+        # update canvas size with image props
         self.canvas.config(width=new_w, height=new_h)
-        self.canvas.delete("all")
-        self.canvas.create_image(0, 0, anchor="nw", image=self.photo)
+        
+        #disable timer after resize image
+        if self.resize_job:
+            self.master.after_cancel(self.resize_job)
+            
+        self.rise_handeler_to_top()
 
-        if self.cell_width:
-            self.draw_grid()
+        #if self.cell_width:
+        #    self.draw_grid()
 
     # ------------------------------------------------------------
     # Обработка изменения размера окна
     # ------------------------------------------------------------
     def on_resize(self, event):
+        
         self.update_scaled_image()
+        
+        # if timer running, when new call happen
+        if self.resize_job:
+            # after_cancel() - cancel timer call, by reference
+            self.master.after_cancel(self.resize_job)
+        
+        # after(time_ms, callback_f()) - start new timer, triggered after time passed
+        self.resize_job = self.master.after(100, lambda: self.update_scaled_image(fast=False))
 
     # ------------------------------------------------------------
     # Калибровка сетки
     # ------------------------------------------------------------
     def start_calibration(self):
+        
         letters_range = simpledialog.askstring("Диапазон букв", "Введите диапазон букв (например A-H):")
-        numbers_range = simpledialog.askinteger("Диапазон цифр", "Введите количество строк (например 20):")
+        numbers_range = simpledialog.askstring("Диапазон цифр", "Введите диапазон цифр (например 1-20):")
 
         start_letter, end_letter = letters_range.split("-")
         letters_raw = [chr(i) for i in range(ord(start_letter), ord(end_letter) + 1)]
@@ -195,83 +263,47 @@ class GridAnnotator:
         # Исключаем буквы Ё и Й
         self.letters = [L for L in letters_raw if L not in ("Ё", "Й")]
 
-        start_num = 1
-        end_num = numbers_range
+        start_num, end_num = numbers_range.split("-")
         self.numbers = list(range(int(start_num), int(end_num) + 1))
         
-        self.create_handles()
+        self.update_grid_preview()
 
     # ------------------------------------------------------------
     # Обработка кликов
     # ------------------------------------------------------------
     def on_click(self, event):
+        
         orig_x = int(event.x / self.scale)
         orig_y = int(event.y / self.scale)
+        
+        current = self.canvas.find_withtag("current")
+        if current:
+            tags = self.canvas.gettags(current[0])
+            square_name = tags[1]
+            if event.num == 1:
+                print(f"Вы выбрали квадрат: {square_name}")
+            if event.num == 3:
+                self.canvas.delete(square_name)
+            
+            
 
-        if self.click_stage == 1:
+        """"                            
+        dlg = simpledialog.askstring("Слой", "Введите слой: ")
+        layer = dlg
 
-            return
+        allowed = messagebox.askyesno(
+            "Использование квадрата",
+            f"Можно использовать квадрат {square_name}?"
+        )
 
-        if self.click_stage == 2:
+        self.grid_data[square_name] = {
+            "layer": layer,
+            "allowed": allowed
+        }
+        """
 
-            return
+        return
 
-        if self.click_stage == 0 and self.cell_width:
-
-            for i, letter in enumerate(self.letters):
-                for j, number in enumerate(self.numbers):
-
-                    x1 = self.grid_left + i * self.cell_width
-                    y1 = self.grid_top + j * self.cell_height
-                    x2 = x1 + self.cell_width
-                    y2 = y1 + self.cell_height
-
-                    if x1 <= orig_x <= x2 and y1 <= orig_y <= y2:
-                        square_name = f"{letter}{number}"
-                        print(f"Вы выбрали квадрат: {square_name}")
-
-                        
-                        dlg = simpledialog.askstring("Слой", "Введите слой: ")
-                        layer = dlg
-
-                        allowed = messagebox.askyesno(
-                            "Использование квадрата",
-                            f"Можно использовать квадрат {square_name}?"
-                        )
-
-                        self.grid_data[square_name] = {
-                            "layer": layer,
-                            "allowed": allowed
-                        }
-
-                        sx1 = int(x1 * self.scale)
-                        sy1 = int(y1 * self.scale)
-                        sx2 = int(x2 * self.scale)
-                        sy2 = int(y2 * self.scale)
-
-                        self.canvas.create_rectangle(sx1, sy1, sx2, sy2, outline="green", width=3)
-                        return
-
-    # ------------------------------------------------------------
-    # Рисование сетки
-    # ------------------------------------------------------------
-    def draw_grid(self):
-        for i, letter in enumerate(self.letters):
-            for j, number in enumerate(self.numbers):
-
-                x1 = self.grid_left + i * self.cell_width
-                y1 = self.grid_top + j * self.cell_height
-                x2 = x1 + self.cell_width
-                y2 = y1 + self.cell_height
-
-                sx1 = int(x1 * self.scale)
-                sy1 = int(y1 * self.scale)
-                sx2 = int(x2 * self.scale)
-                sy2 = int(y2 * self.scale)
-
-                self.canvas.create_rectangle(sx1, sy1, sx2, sy2, outline="red")
-                self.canvas.create_text(sx1 + 5, sy1 + 5, anchor="nw",
-                                        text=f"{letter}{number}", fill="blue")
 
     # ------------------------------------------------------------
     # Сохранение JSON
@@ -286,30 +318,6 @@ class GridAnnotator:
 
         print("JSON сохранён:", file_path)
     
-    # ------------------------------------------------------------
-    # Полный сброс сетки и данных.
-    # ------------------------------------------------------------   
-    def reset_grid(self):
-
-        self.grid_left = None
-        self.grid_top = None
-        self.cell_width = None
-        self.cell_height = None
-
-        self.letters = []
-        self.numbers = []
-        self.grid_data = {}
-
-        self.click_stage = 0
-
-        # Очищаем Canvas
-        self.canvas.delete("all")
-
-        # Перерисовываем изображение, если оно загружено
-        if self.photo:
-            self.canvas.create_image(0, 0, anchor="nw", image=self.photo)
-
-        print("Сетка сброшена. Можно начать калибровку заново.")
 
 # ------------------------------------------------------------
 # Запуск
